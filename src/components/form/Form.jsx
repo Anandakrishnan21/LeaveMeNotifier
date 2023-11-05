4e497a84e700390ea7ec3836d7b1946633af91e6
Anandakrishnan Dinej <anandakrishnandinej02@gmail.com>
2023-11-05 21:07:37 +0530

    form, home page updated

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -95,7 +95,7 @@ function Form() {
     <div className="bg-gradient-to-r from-indigo-50 via-indigo-100 to-indigo-50 flex flex-col items-center">
       <form className="font-custom text-sm w-5/6 md:w-5/12 bg-white p-2 border-2 border-gray-200 flex flex-col gap-6 my-10 py-10 rounded-xl">
         <div className="w-5/6 flex flex-col m-auto gap-2">
-          <h1 className="text-lg font-semibold">Sender's Address Details</h1>
+          <h1 className="text-lg font-semibold">Sender&apos;s Address Details</h1>
           {FormInput1.map((input) => (
             <div key={input.id}>
               <label htmlFor={input.labelFor}>{input.labelName}</label>
@@ -112,7 +112,7 @@ function Form() {
           ))}
         </div>
         <div className="w-5/6 flex flex-col m-auto gap-2">
-          <h1 className="text-lg font-semibold">Receiver's Address Details</h1>
+          <h1 className="text-lg font-semibold">Receiver&apos;s Address Details</h1>
           {FormInput2.map((input) => (
             <div key={input.id}>
               <label htmlFor={input.labelFor}>{input.labelName}</label>
--- src/components/homePage/HomePageContent.jsx
+++ src/components/homePage/HomePageContent.jsx
@@ -5,7 +5,7 @@ function HomePageContent() {
   return (
     <div className="font-custom w-5/6 md:w-1/2 flex flex-col text-base gap-2">
       <h1 className="text-3xl md:text-5xl font-medium leading-normal md:leading-relaxed">
-        Create Custom Letter's in Minutes
+        Create Custom Letter&apos;s in Minutes
       </h1>
       <p className="text-sm md:text-base leading-normal">
         Unlock the power of effortless Pdf of letters creation with Letter Wizard - your
