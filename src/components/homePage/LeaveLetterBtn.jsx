41d6aa409f12a2f62a4a69e8ab69b79d36868a10
Anandakrishnan Dinej <anandakrishnandinej02@gmail.com>
2023-11-06 20:00:04 +0530

    form, home page, leave btn updated

--- src/components/homePage/LeaveLetterBtn.jsx
+++ src/components/homePage/LeaveLetterBtn.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function LeaveLetterBtn() {
   return (
     <Link href="/form">
-      <button className="w-1/2 bg-slate-500 hover:bg-slate-400 p-1.5 text-white border-2 duration-500 text-sm md:text-base rounded">
+      <button className="w-1/2 bg-slate-500 hover:bg-slate-400 p-1.5 px-2 text-white border-2 duration-500 text-sm md:text-base rounded">
         Create Leave Letter
       </button>
     </Link>
